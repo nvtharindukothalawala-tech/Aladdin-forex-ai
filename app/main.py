a50372e091d132f11518768bfd7bf799525fcc10
Tharindu Kothalawala <nv.tharindukothalawala@gmail.com>
2026-07-18 19:17:14 +0530

    Add input validation using if statement

--- app/main.py
+++ app/main.py
@@ -3,10 +3,13 @@ print("=== Aladdin Forex Risk Calculator ===")
 balance = float(input("Enter account balance: "))
 risk_percentage = float(input("Enter risk percentage: "))
 
-risk_amount = balance * risk_percentage / 100
+if balance > 0:
+    risk_amount = balance * risk_percentage / 100
 
-print()
-print("------ Result ------")
-print("Account Balance:", balance)
-print("Risk Percentage:", risk_percentage, "%")
-print("Risk Amount:", risk_amount)
+    print()
+    print("------ Result ------")
+    print("Account Balance:", balance)
+    print("Risk Percentage:", risk_percentage, "%")
+    print("Risk Amount:", risk_amount)
+else:
+    print("Error: Account balance must be greater than 0.")
