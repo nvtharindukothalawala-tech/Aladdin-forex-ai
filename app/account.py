d1c0b371a1b810793dda0531ad8765ce7d68e124
Tharindu Kothalawala <nv.tharindukothalawala@gmail.com>
2026-07-18 21:25:46 +0530

    Learn Python OOP basics

---
+++ app/account.py
@@ -0,0 +1,6 @@
+class TradingAccount:
+
+    def __init__(self, name, balance, risk):
+        self.name = name
+        self.balance = balance
+        self.risk = risk
--- app/main.py
+++ app/main.py
@@ -1,44 +1,11 @@
-from risk import calculate_risk
+from account import TradingAccount
 
-accounts = [
-    {
-        "name": "Main Account",
-        "balance": 5000,
-        "risk": 2
-    },
-    {
-        "name": "Swing Account",
-        "balance": 8000,
-        "risk": 1.5
-    },
-    {
-        "name": "Demo Account",
-        "balance": 10000,
-        "risk": 1
-    },
-    {
-        "name": "Invalid Account",
-        "balance": -3000,
-        "risk": 2
-    }
-]
+account = TradingAccount(
+    "Main Account",
+    5000,
+    2
+)
 
-print("=== Aladdin Risk Report ===")
-print()
-
-for account in accounts:
-    risk_amount = calculate_risk(
-        account["balance"],
-        account["risk"]
-    )
-
-    print("Account:", account["name"])
-
-    if risk_amount is None:
-        print("Status: Invalid account balance")
-    else:
-        print("Balance:", account["balance"])
-        print("Risk:", account["risk"], "%")
-        print("Risk Amount:", risk_amount)
-
-    print()
+print("Account:", account.name)
+print("Balance:", account.balance)
+print("Risk:", account.risk, "%")
