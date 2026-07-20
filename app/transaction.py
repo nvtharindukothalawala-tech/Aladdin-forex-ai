ce5d7ca5a52329c57aa1b75085eb0e47fea1c5bb
Tharindu Kothalawala <nv.tharindukothalawala@gmail.com>
2026-07-20 19:07:19 +0530

    Add transaction IDs to transaction history

--- app/transaction.py
+++ app/transaction.py
@@ -2,13 +2,19 @@ from datetime import datetime
 
 class Transaction:
 
+    next_id = 1
+
     def __init__(self, transaction_type, account, amount):
+        self.transaction_id = f"TX{Transaction.next_id:03d}"
+        Transaction.next_id += 1
+
         self.transaction_type = transaction_type
         self.account = account
         self.amount = amount
         self.timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     def show_details(self):
+        print("Transaction ID:", self.transaction_id)
         print("Date & Time:", self.timestamp)
         print("Transaction Type:", self.transaction_type)
         print("Account:", self.account)
