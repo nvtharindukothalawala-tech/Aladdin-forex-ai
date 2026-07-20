57a56c4554b849f36b48be5bb77009f02c2185bc
Tharindu Kothalawala <nv.tharindukothalawala@gmail.com>
2026-07-20 18:51:06 +0530

    Add timestamps to transaction history

--- app/transaction.py
+++ app/transaction.py
@@ -1,11 +1,15 @@
+from datetime import datetime
+
 class Transaction:
 
     def __init__(self, transaction_type, account, amount):
         self.transaction_type = transaction_type
         self.account = account
         self.amount = amount
+        self.timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     def show_details(self):
+        print("Date & Time:", self.timestamp)
         print("Transaction Type:", self.transaction_type)
         print("Account:", self.account)
         print("Amount:", self.amount)
